316538fec5932d4508401444c4ba65baf5e72d84
Spafi <63115231+Spafi@users.noreply.github.com>
2021-04-15 17:31:27 +0300

    Add get method in job controller

--- src/main/java/com/jobfull/jobFull/jobPostManagement/controller/JobPostController.java
+++ src/main/java/com/jobfull/jobFull/jobPostManagement/controller/JobPostController.java
@@ -1,11 +1,15 @@
 package com.jobfull.jobFull.jobPostManagement.controller;
 
 
+import com.jobfull.jobFull.jobPostManagement.model.JobPostModel;
 import com.jobfull.jobFull.jobPostManagement.service.JobPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/jobs")
@@ -19,4 +23,8 @@ public class JobPostController {
         this.jobService = jobService;
     }
 
+    @GetMapping
+    public List<JobPostModel> getAll() {
+        return jobService.getAll();
+    }
 }
